b217630547f827fac579e18a60a1f6676d0e2290
Geetanjali <72863114+geetanjalis22@users.noreply.github.com>
2025-12-23 17:38:53 +0530

    Add registration path to urlpatterns

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,7 +10,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='register', view=views.registration, name='register'),
     # path for login/logout
     path(route='login', view=views.login_user, name='login'),
     path('logout/', views.logout_request, name='logout'),
