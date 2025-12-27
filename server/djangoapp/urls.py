e16171e51c241cfb64b8d340c15d5daaf35e8c30
Geetanjali <72863114+geetanjalis22@users.noreply.github.com>
2025-12-27 15:39:39 +0530

    Add route for getting cars in urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # path for login/logout
     path(route='login', view=views.login_user, name='login'),
     path('logout/', views.logout_request, name='logout'),
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
